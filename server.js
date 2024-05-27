5327bb367336df09912284962b7bcfc4243bdbfd
elia1020 <tikshov1312@gmail.com>
2024-05-27 13:40:55 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import products from "./routes/product.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/product", products);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
